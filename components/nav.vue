7634b1ffd097fa34ebb5632bfc895e18400ebd2c
Raquib-ul Alam <alam.kanak@gmail.com>
2024-11-12 14:55:26 -0500

    Update toolbar

---
+++ components/nav.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul
+    class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white"
+  >
+    <li>
+      <NuxtLink
+        to="/"
+        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+        >Home</NuxtLink
+      >
+    </li>
+    <li>
+      <NuxtLink
+        to="/privacy"
+        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+        >Privacy Policy</NuxtLink
+      >
+    </li>
+    <li>
+      <NuxtLink
+        to="/terms"
+        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+        >Terms & Conditions</NuxtLink
+      >
+    </li>
+    <li>
+      <NuxtLink
+        to="/delete"
+        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+        >Delete Account</NuxtLink
+      >
+    </li>
+    <li>
+      <NuxtLink
+        to="mailto:alam.kanak@gmail.com"
+        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+        >Contact Us</NuxtLink
+      >
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.router-link-active {
+  @apply text-white bg-blue-700 md:bg-transparent md:text-blue-700;
+}
+</style>
--- components/toolbar.vue
+++ components/toolbar.vue
@@ -3,19 +3,12 @@
     <div
       class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
     >
-      <a
-        href="https://flowbite.com/"
-        class="flex items-center space-x-3 rtl:space-x-reverse"
-      >
-        <NuxtImg
-          src="icon.png"
-          class="h-8"
-          alt="Flowbite Logo"
-        />
+      <NuxtLink to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <NuxtImg src="icon.png" class="h-8" alt="Flowbite Logo" />
         <span class="self-center text-2xl font-semibold whitespace-nowrap"
           >Art Guide</span
         >
-      </a>
+      </NuxtLink>
       <button
         data-collapse-toggle="navbar-default"
         type="button"
@@ -40,53 +33,10 @@
           />
         </svg>
       </button>
+
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul
-          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white"
-        >
-          <li>
-            <NuxtLink
-              to="/"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-              >Home</NuxtLink
-            >
-          </li>
-          <li>
-            <NuxtLink
-              to="/privacy"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-              >Privacy Policy</NuxtLink
-            >
-          </li>
-          <li>
-            <NuxtLink
-              to="/terms"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-              >Terms & Conditions</NuxtLink
-            >
-          </li>
-          <li>
-            <NuxtLink
-              to="/delete"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-              >Delete Account</NuxtLink
-            >
-          </li>
-          <li>
-            <NuxtLink
-              to="mailto:alam.kanak@gmail.com"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-              >Contact Us</NuxtLink
-            >
-          </li>
-        </ul>
+        <Nav></Nav>
       </div>
     </div>
   </nav>
 </template>
-
-<style scoped>
-.router-link-active {
-  @apply text-white bg-blue-700 md:bg-transparent md:text-blue-700;
-}
-</style>
