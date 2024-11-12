9e6809f658fa45ba00b6480cd9809adb27e531fe
Raquib-ul Alam <alam.kanak@gmail.com>
2024-11-12 03:41:11 -0500

    Upgrade to nuxt

---
+++ components/footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="py-10 flex flex-col items-center">
+    <div class="flex justify-center gap-3 font-medium mb-4">
+      <NuxtLink to="/" class="text-blue-700 hover:underline">Home</NuxtLink>
+      <div class="text-gray-300">|</div>
+      <NuxtLink to="/privacy" class="text-blue-700 hover:underline"
+        >Privacy Policy</NuxtLink
+      >
+      <div class="text-gray-300">|</div>
+      <NuxtLink to="/terms" class="text-blue-700 hover:underline"
+        >Terms & Conditions</NuxtLink
+      >
+      <div class="text-gray-300">|</div>
+      <NuxtLink
+        to="mailto:alam.kanak@gmail.com"
+        class="text-blue-700 hover:underline"
+        >Contact Us</NuxtLink
+      >
+    </div>
+    <div class="text-gray-400">
+        Copyright © {{ new Date().getFullYear() }} Raquib Ul Alam.
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="container mx-auto px-4">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-6 items-center">
+        <NuxtImg class="w-full mb-4" src="/screenshot.png" />
+        <div class="flex flex-col items-center">
+          <NuxtImg class="mb-4" src="/icon.png" />
+          <h1 class="text-5xl font-bold mb-4">Art Guide</h1>
+          <h2 class="text-center text-xl mb-4">
+            Learn how to draw in clear step by step process
+          </h2>
+          <p class="text-center text-gray-500">
+            Easy step by step drawing tutorials for beginners. Learn how to draw
+            and paint using animations and voice guide. Best for beginners and
+            new learners.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="bg-gray-200 py-10">
+      <div class="container mx-auto">
+        <p>
+          Easy step by step drawing tutorials for beginners. Learn how to draw
+          and paint using animations and voice guide. Best for beginners and new
+          learners.
+        </p>
+        <p>Want to be the future Bob Ross? Start here.</p>
+        <p class="font-semibold mt-4 mb-1 text-lg">● ART TUTORIALS</p>
+        <p>
+          Learn how to draw and paint with clear, step-by-step
+          tutorials/instructions and voiceovers. Learning drawing and painting
+          has never been so easy.
+        </p>
+        <p class="font-semibold mt-4 mb-1 text-lg">● LARGE CATALOG</p>
+        <p>
+          Browse a catalog of free and paid handcrafted drawing tutorials
+          including flowers, anime, cartoons, animals, portraits, human body,
+          face, landscapes, still life, etc.
+        </p>
+        <p class="font-semibold mt-4 mb-1 text-lg">● VOICE GUIDE</p>
+        <p>
+          Each tutorial is crafted carefully by expert artists with interactive
+          animations and computerized voiceovers.
+        </p>
+        <p class="font-semibold mt-4 mb-1 text-lg">● AUTOMATIC ASSESSMENT</p>
+        <p>
+          Use our automatic art comparison tool to see how good you are getting
+          in art.
+        </p>
+        <p class="font-semibold mt-4 mb-1 text-lg">● ART GUIDE PREMIUM</p>
+        <p class="mb-3">
+          Unlock the entire catalog of art tutorials with just one subscription.
+          Boost your art skills by practicing daily. New artworks are added
+          regularly and unlocked automatically for you..
+        </p>
+        <p>
+          Thank you for supporting by using our new app. Feel free to reach out
+          to us at app.artguide@gmail.com if you have any feature request. We
+          are extensively working on this app to bring you new features. We are
+          planning to add more features for painting, watercolor etc.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
